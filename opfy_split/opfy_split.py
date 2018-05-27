b1baef0f5103ef41ca67ff202d04a06a4f0747c6
Luiz Felix <lzcfelix@gmail.com>
2018-05-27 17:41:05 -0300

    Improving docstrings.

--- opfy_split/opfy_split.py
+++ opfy_split/opfy_split.py
@@ -1,67 +1,86 @@
 import os
 import pickle
 
-import numpy as np
-
 
 def _restore_files(filepath):
-    """Given a fold file, returns its X matrix, y vector and the filepath."""
-    
+    """
+    Given a fold pickle file, returns its correponding X matrix, y vector and
+    filepath argument.
+
+    :param filepath: Path to the pickle representation of an OPF fold .dat file
+    :returns: A tuple (X, y, filepath).
+    """
+
     with open(filepath, 'rb') as file:
         data = pickle.load(file)
         X, y = data['X'], data['y']
-        
+
     return X, y, filepath
 
 
 def _read_pickle(folds_folder, filepath, index):
-    """Restore the dev/test fold for a given cross-validation split.
-    
-    > folds_folder: Path to the folder containing all folds.
-    > filepath: The cross-validation file.
-    > index: The index that determines the test/dev fold id.
-    < X, y and the fold filename
     """
-    
-    # keep just the filename (ignore possible extension) and identify
-    # the corresponding test/dev split id
+    Restore either the correponding pickle file corresponding to the dev or
+    test related to a given cross-validation fold.
+
+    :param folds_folder: Path to the folder containing all the pickle objects
+    corresponding to OPF .dat files.
+    :param filepath: The cross-validation pickle filename.
+    :param index: The index that determines which number from the filepath
+    should be used to restore the desired data partition.
+    :returns: A tuple (X, y, f), where f is the filename of the restored pickle
+    file from the disk.
+    """
+
+    # keep just the filename (ignore possible extension) and identify the
+    # corresponding test/dev split id
     filepath = os.path.basename(filepath)
     index = filepath.split('_')[index]
     index = index.split('.')[0]
-    
-    # restoring the fold filename for numpy
+
+    # restoring the fold filename for numpy and loading features and labels
     filename = 'fold_{}.pyk'.format(index)
-    
-    # load the features and labels
     return _restore_files(os.path.join(folds_folder, filename))
 
 
 def get_dev_fold(folds_folder, filepath):
-    """Restores the dev X matrix and y vector for a cross-validation filename.
-    
-    > folds_folder: Path to the folder containing all folds.
-    > filepath: Cross-validation split filename.
-    < X, y, filename of the corresponding dev fold.
+    """
+    Restores the corresponding dev X, y components of a cross-validation fold
+    from a pickle file.
+
+    :param folds_folder: Path to the folder containing all the pickle objects
+    corresponding to OPF .dat files.
+    :param filepath: The cross-validation pickle filename.
+    :returns: A tuple (X, y, f), where f is the filename of the restored pickle
+    file.
     """
     return _read_pickle(folds_folder, filepath, 3)
 
 
 def get_test_fold(folds_folder, filepath):
-    """Restores the test X matrix and y vector for a cross-validation filename.
-    
-    > folds_folder: Path to the folder containing all folds.
-    > filepath: Cross-validation split filename.
-    < X, y, filename of the corresponding dev fold.
+    """
+    Restores the corresponding test X, y components of a cross-validation
+    fold from a pickle file.
+
+    :param folds_folder: Path to the folder containing all the pickle objects
+    corresponding to OPF .dat files.
+    :param filepath: The cross-validation pickle filename.
+    :returns: A tuple (X, y, f), where f is the filename of the restored pickle
+    file.
     """
     return _read_pickle(folds_folder, filepath, 5)
 
 
 def fetch_folds(path):
-    """Returns a generator that at each step restores one cross-validation fold.
-    > path: Path to the folder with all folds for cross-validating.
-    < X, y, filename of the corresponding training fold.
+    """
+    Returns a generator that, at each step, restores one cross-validation
+    fold from the pickle files on the disk.
+
+    :param path: Path to the folder with all folds for cross-validation pickle
+    files.
+    :returns A tuple (X, y, f) of the corresponding training part of the cross-
+    validation fold, where f is the name of the restored file.
     """
     files = sorted(os.listdir(path))
     for file in files:
         yield _restore_files(os.path.join(path, file))
-
